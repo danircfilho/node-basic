eb1598fe6de23e29ac1cf1ed8efc7add3a2d12b7
danircfilho <danircfilho@hotmail.com>
2021-12-07 17:40:52 -0300

    check balance-deposit-withdraw money

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 - Small project that simulates access to a bank account. The simulation takes place directly at the prompt.
 
-Copyrigth (c) 2021 - Daniel Rodigues de Carvalho Filho
+Copyrigth (c) 2021 - Daniel Rodrigues de Carvalho Filho
 
 "This project is licensed under the terms of the MIT license."
 
--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const fs = require('fs')
 
 console.log('Programa Iniciado!')
 
-//Invoke function
+//invoke function
 operation()
 
 function operation() {
@@ -25,13 +25,204 @@ function operation() {
 
         if(action === 'Create an account') {
             createAccount()
+        } else if (action === 'Check balance') {
+            getAccountBalance()
+        } else if (action === 'Deposit') {
+            deposit()
+        } else if (action === 'Withdraw money') {
+            withdraw()
+        } else if (action === 'Quit') {
+            console.log(chalk.bgBlue.black('Thank you for using this software!'))
+            process.exit() //terminate the system
         }
     })
     .catch((err) => console.log(err))
 }
 
-//Criar a conta bancária
+//create an account
 function createAccount() {
-    console.log(chalk.bgGreen.black("Congratulations on choosing our bank!"))
-    console.log(chalk.green("Set your account options below"))
+    console.log(chalk.bgGreen.black('Congratulations on choosing our bank!'))
+    console.log(chalk.green('Set your account options below'))
+
+    buildAccount()
+    return
 }
+
+function buildAccount() {
+    inquirer.prompt([
+        {
+            name: 'accountName',
+            message: 'Enter a name for your account',
+        },
+    ])
+    .then((answer) => {
+        const accountName = answer ['accountName']
+
+        console.info(accountName)
+
+        if(!fs.existsSync('accounts')) {
+            fs.mkdirSync('accounts')
+        }   
+        
+        if(fs.existsSync(`accounts/${accountName}.json`)) {
+
+            console.log(
+                chalk.bgRed.black('This account already exists. choose another name!')
+            )
+
+            buildAccount()
+        }   
+
+        fs.writeFileSync(`accounts/${accountName}.json`, '{"balance": 0}', 
+            function (err) {
+                console.log(err)
+            },
+        )
+
+        console.log(
+            chalk.green('Congratulations, your account has been created!')
+        )
+        
+        operation()
+    })
+    .catch((err) => console.log(err))
+}
+
+//add values to account
+function deposit() {
+    inquirer.prompt([
+        {
+            name: 'accountName',
+            message: 'what is your account name?',
+        },
+    ])
+    .then((answer) => {
+        const accountName = answer ['accountName']
+
+        //check if the account exists
+        if(!checkAccount(accountName)) {
+            return deposit()
+        }
+
+        inquirer.prompt([
+            {
+                name: 'amount',
+                message: 'How much do you want to deposit?',
+            },
+        ])
+        .then((answer) => {
+            const amount = answer['amount']
+
+            //add an amount
+            addAmount(accountName, amount)
+            operation()
+        })
+        .catch((err) => console.log(err))
+    })
+    .catch((err) => console.log(err))
+}
+
+//check if the account exists
+function checkAccount(accountName) {
+
+    if(!fs.existsSync(`accounts/${accountName}.json`)) {
+        console.log(chalk.bgRed.black('This account does not exist. Choose another one.'))
+        return false
+    }
+    //proceed to code if account exists
+    return true
+}
+
+function addAmount (accountName, amount) {
+    const accountData = getAccount(accountName) 
+    
+    if(!amount) {
+        console.log(
+            chalk.bgRed.black('An error has occurred! Try again later.'
+        ))
+        return deposit()
+    }
+    accountData.balance = parseFloat(amount) + parseFloat(accountData.balance)
+
+    fs.writeFileSync(
+        `accounts/${accountName}.json`, 
+        JSON.stringify(accountData),
+        function (err) {
+            console.log(err)
+        },
+    )
+        console.log(
+            chalk.green(`Has been deposited R$${amount} into your account!`),
+        )
+}
+
+function getAccount (accountName) {
+    const accountJSON = fs.readFileSync(`accounts/${accountName}.json`, {
+        encoding: 'utf8',
+        flag: 'r', //read
+    })
+    return JSON.parse(accountJSON)
+}
+
+//account balance
+function getAccountBalance() {
+    inquirer.prompt([
+        {
+            name: 'accountName',
+            message: 'what is your account name?',
+        },
+    ])
+    .then((answer) => {
+        const accountName = answer['accountName']
+
+        //check if the account exists
+        if(!checkAccount(accountName)) {
+            return getAccountBalance()
+        }
+        
+        const accountData = getAccount(accountName)
+
+
+        console.log(chalk.bgBlue.black(
+            `Your account balance is R$${accountData.balance}`
+        ))
+        operation()
+    })  
+    .catch(err => console.log(err))
+}
+
+//withdraw amounts from the account
+function withdraw() {
+    inquirer.prompt([
+        {
+            name: 'accountName',
+            message: 'what is your account name?',
+        },
+    ])
+    .then((answer) => {
+        accountName = answer['accountName']
+
+        //check if the account exists
+        if(!checkAccount(accountName)) {
+            return withdraw()
+        }
+
+        inquirer.prompt([
+            {
+                name: 'accountName',
+                message: 'How much do you want to withdraw?',
+            },
+        ])
+        .then((answer) => {
+            const amount = answer['amount']
+
+            removeAmount(accountName, amount)
+
+            operation()
+        })
+        .catch(err => console.log(err))
+    })
+    .catch(err => console.log(err))
+}
+
+function removeAmount(accountName, amount)
